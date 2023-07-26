28c083f99a9b2b36cc60e660bb6c7602623f2d94
서민지 <s__min_ji@naver.com>
2023-07-26 17:53:55 +0900

    1

---
+++ minji/swea/game.py
@@ -0,0 +1,21 @@
+a, b = map(int, input().split())
+
+if a == 1:
+    if b == 2:
+        print('B')
+    if b == 3:
+        print('A')
+        
+        
+if a == 2:
+    if b == 1:
+        print('A')
+    if b == 3:
+        print('B')
+        
+        
+if a == 3:
+    if b == 1:
+        print('B')
+    if b == 2:
+        print('A')
