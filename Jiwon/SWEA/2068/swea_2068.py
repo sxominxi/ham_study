f65c136c2379bdd6305e188016a89507048e896f
Jiwon <yjw5602@naver.com>
2023-07-27 19:48:19 +0900

    ham

--- Jiwon/SWEA/2068/swea_2068.py
+++ Jiwon/SWEA/2068/swea_2068.py
@@ -2,7 +2,7 @@ import sys
 sys.stdin = open('input.txt')             # input 파일 불러오기
 
 T = int(input())                          # 3줄 (3번) 실행
-for number in range(T):                   # 한 줄씩 돌아가며
+for number in range(1, T+1):                   # 한 줄씩 돌아가며 , range(1, T+1)로 적고 마지막줄 print에서 {number+1}대신 {number}로 하는게 좋음
     numbers = list(map(int, input().split()))  # 한 줄에 있는것들을 공백으로 나눠서 int로 다 바꿈
     big = 0                               # 대소 비교를 위한 변수, 큰 수를 저장할 곳
 
@@ -10,7 +10,7 @@ for number in range(T):                   # 한 줄씩 돌아가며
         if big < i:                       # 돌아가면서(for) 대수 비교
             big =  i                      # 크면 big에 큰 수 저장 (갱신)
 
-    print(f'#{number+1} {big}')           # number는 0, 1, 2
+    print(f'#{number} {big}')           # number는 0, 1, 2
 
 
     
